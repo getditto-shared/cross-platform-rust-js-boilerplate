de5de2597e8a4081ef223135ceb362b5afb334e5
Konstantin Bender <me@konstantinbender.com>
2020-11-04 17:44:01 +0100

    Remove new method from Store trait

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -2,14 +2,7 @@ use async_trait::async_trait;
 
 #[async_trait(?Send)]
 pub trait Store {
-    // We are using type erasure (dyn Store) in `JSStore` to retain a
-    // handle to 'any Store'. Therefore, we apparently can't have a constructor
-    // as part of the trait (size can't be known at compile time). Not sure if
-    // there is a way to square this circle, until then, commented out:
-    // fn new(name: &str) -> Self;
-
     async fn get(&self, key: &str) -> Result<Option<String>, ()>;
     async fn put(&mut self, key: &str, value: &str) -> Result<(), ()>;
-
     async fn clear(&mut self) -> Result<(), ()>;
 }
